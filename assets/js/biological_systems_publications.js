0d1aadb09bc8e16f4db61c2a8f9dd67eedec53e1
sdivi <sdivi@andrew.cmu.edu>
2021-03-19 13:05:16 -0400

    Updating members page info

--- assets/js/biological_systems_publications.js
+++ assets/js/biological_systems_publications.js
@@ -1,11 +1,87 @@
 const papers =  {
-
- yNote: [{
-	    image: "images/",
-        authors: ["To be added"],
-        title: "Under construction",
-        link: "#",
-        journal: "Please check back later...",
-        page:""
-	}
- ]}
+    y2021: [{
+        image: "images/",
+        authors: ["S.J. Longo", "W. Ray", "G.M. Farley", "J. Harrison", "J. Jorge", "T. Kaji", "A.R. Palmer", "S.N. Patek"],
+        title: "Snaps of a tiny amphipod push the boundary of ultrafast, repeatable movement", 
+        link: "https://www.sciencedirect.com/science/article/pii/S0960982220318777",
+        journal: "Current Biology",
+        page:"31(3), pp.R116-R117." 
+    },{
+        image: "images/",
+        authors: ["J.F.Jorge", "S. Bergbreiter", "S.N. Patek"],
+        title: "Pendulum-based measurements reveal impact dynamics at the scale of a trap-jaw ant", 
+        link: "https://jeb.biologists.org/content/224/5/jeb232157",
+        journal: "Journal of Experimental Biology",
+        page:"jeb.232157, doi: 10.1242/jeb.232157.",
+		doi: ""
+    }
+	
+], y2020: [{
+        image: "images/",
+        authors: ["Elizabeth Mendoza", "Emanuel Azizi", "Daniel S. Moen"],
+        title: "What explains vast differences in jumping power within a clade? Diversity, ecology and evolution of anuran jumping power",
+        link: "https://besjournals.onlinelibrary.wiley.com/doi/10.1111/1365-2435.13545",
+        journal: "Functional Ecology",
+        page:"34: 1053-1063."
+    }
+	
+], y2019: [{
+        image: "images/",
+        authors: ["Joseph R. Schulz", "Ian Jan", "Gurleen Sangha", "Emanuel Azizi"],
+        title: "The high speed radular prey strike of a fish-hunting cone snail", 
+        link: "https://www.sciencedirect.com/science/article/pii/S0960982219308796",
+        journal: "Current Biology",
+        page:"29(16), pp. R788-R789." 
+    },{
+        image: "images/",
+        authors: ["S.J. Longo", "S.M. Cox", "E. Azizi", "M. Ilton", "J.P. Olberding", "R. St Pierre", "S.N. Patek"],
+        title: "Beyond power amplification: latch-mediated spring actuation is an emerging framework for the study of diverse elastic systems",
+        link: "https://jeb.biologists.org/content/222/15/jeb197889",
+        journal: "Journal of Experimental Biology",
+        page:"222:jeb197889."
+    },{
+        image: "images/",
+        authors: ["Gregory P Sutton", "Elizabeth Mendoza", "Emanuel Azizi", "Sarah J Longo", "Jeffrey P Olberding", "Mark Ilton", "Sheila N Patek"],
+        title: "Why do Large Animals Never Actuate Their Jumps with Latch-Mediated Springs? Because They can Jump Higher Without Them ",
+        link: "https://academic.oup.com/icb/article/59/6/1609/5545545",
+        journal: "Integrative and Comparative Biology",
+        page:"59(6), pp. 1609-1618."
+    },{
+        image: "images/",
+        authors: ["Jeffrey P Olberding", "Stephen M Deban", "Michael V Rosario", "Emanuel Azizi"],
+        title: "Modeling the Determinants of Mechanical Advantage During Jumping: Consequences for Spring- and Muscle-Driven Movement",
+        link: "https://academic.oup.com/icb/article/59/6/1515/5545521",
+        journal: "Integrative and Comparative Biology",
+        page:"59(6), pp. 1515-1524."
+    },{
+        image: "images/",
+        authors: ["G.M. Farley", "M.J. Wise", "J.S. Harrison", "G.P. Sutton", "C. Kuo", "S.N. Patek"],
+        title: "Adhesive latching and legless leaping in small, worm-like insect larvae",
+        link: "https://jeb.biologists.org/content/222/15/jeb201129",
+        journal: "Journal of Experimental Biology",
+        page:"222:jeb201129."
+    },{
+        image: "images/",
+        authors: ["S.N. Patek"],
+        title: "The Power of Mantis Shrimp Strikes: Interdisciplinary Impacts of an Extreme Cascade of Energy Release",
+        link: "https://academic.oup.com/icb/article/59/6/1573/5531653",
+        journal: "Integrative and Comparative Biology",
+        page:"59(6), pp. 1573-1585."
+    },{
+        image: "images/",
+        authors: ["Crystal M Reynaga", "Caitrin E Eaton", "Galatea A Strong", "Emanuel Azizi"],
+        title: "Compliant Substrates Disrupt Elastic Energy Storage in Jumping Tree Frogs",
+        link: "https://academic.oup.com/icb/article/59/6/1535/5505430",
+        journal: "Integrative and Comparative Biology",
+        page:"59(6), pp. 1535-1545."
+    }
+	
+], y2018: [{
+        image: "images/",
+        authors: ["Mark Ilton", "M Saad Bhamla", "Xiaotian Ma", "Suzanne M Cox", "Leah L Fitchett","Yongjin Kim","Je-sung Koh","Deepak Krishnamurthy","Chi-Yun Kuo","Fatma Zeynep Temel","Alfred J Crosby","Manu Prakash","Gregory P Sutton","Robert J Wood","Emanuel Azizi","Sarah Bergbreiter","SN Patek"],
+        title: "The principles of cascading power limits in small, fast biological and engineered systems.",
+        link: "https://science.sciencemag.org/content/360/6387/eaao1082.abstract",
+        journal: "Science",
+        page:"360, no. 6387 (2018): eaao1082."
+    }
+]}
